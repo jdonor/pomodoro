f920bd4411409d470306792971264cadf8a39ec5
Josiah Donor <jdonor@gmail.com>
2018-02-15 17:24:22 -0600

    Start timer when the user clicks it (#7)
    * Implement formatDisplay
    
    * Implement Timer class
    
    * Log the value of timeRemaining
    
    * Allow user to click on the timer to start it
    
    * Prevent starting multiple countdowns

--- main.js
+++ main.js
@@ -1,5 +1,88 @@
-function sum(a, b) {
-  return a + b;
+modes = Object.freeze({
+  WORK: 0,
+  BREAK: 1
+});
+
+class Timer {
+  constructor() {
+    this.worktimeLength = 1500;
+    this.breaktimeLength = 300;
+    this.mode = modes.WORK;
+    this._timeRemaining = 1500;
+    this.timeUpdated = () => {};
+    this._paused = true;
+  }
+
+  get timeRemaining() {
+    return this._timeRemaining;
+  }
+
+  set timeRemaining(time) {
+    this._timeRemaining = time;
+    this.timeUpdated();
+  }
+
+  reset() {
+    this.timeRemaining = this.worktimeLength;
+    this.mode = modes.WORK;
+  }
+
+  start() {
+    if (!this._paused) return;
+
+    this._paused = false;
+
+    this.countdown = setInterval(() => {
+      this.timeRemaining = this.timeRemaining - 1;
+
+      if (this.timeRemaining <= 0) {
+        clearInterval(this.countdown);
+      }
+    }, 1000);
+  }
+
+  switchMode() {
+    this.mode = this.mode === modes.WORK ? modes.BREAK : modes.WORK;
+  }
 }
 
-module.exports = sum;
+Number.prototype.pad = function (size = 2) {
+  let s = String(this);
+  while (s.length < size) {
+    s = '0' + s;
+  }
+
+  return s;
+}
+
+function formatDisplay(seconds) {
+  const minutes = Math.trunc(seconds / 60);
+  seconds = seconds % 60;
+  
+  return `${minutes.pad()}:${seconds.pad()}`;
+}
+
+// Node-only code
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+  module.exports = {
+    formatDisplay,
+    Timer,
+    modes
+  };
+} else { // Browser-only code
+  function startTimer() {
+    timer.start();
+  }
+
+  // Wire up to the DOM
+  const timerDiv = document.querySelector('#timer');
+  const time = document.querySelector('#time');
+  const timer = new Timer();
+
+  function updateTime() {
+    time.textContent = formatDisplay(timer.timeRemaining);
+  }
+
+  timer.timeUpdated = updateTime;
+  timerDiv.addEventListener('click', startTimer);
+}
--- tests/main.test.js
+++ tests/main.test.js
@@ -1,5 +1,155 @@
-const sum = require('../main');
+const main = require('../main');
+const {formatDisplay, Timer, modes} = main;
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+jest.useFakeTimers(); // Mock setTimeout and other timer functions
+
+describe('formatDisplay', () => {
+  test('should exist', () => {
+    expect(formatDisplay).toBeDefined();
+  });
+  
+  test('should return a value', () => {
+    expect(formatDisplay(30)).toBeTruthy();
+  });
+
+  test('should format a small value correctly', () => {
+    expect(formatDisplay(30)).toBe('00:30');
+  });
+
+  test('should format a medium value correctly', () => {
+    expect(formatDisplay(95)).toBe('01:35');
+  });
+
+  test('should format a large value correctly', () => {
+    expect(formatDisplay(1500)).toBe('25:00');
+  });
+});
+
+describe('Timer class', () => {
+  let timer = new Timer();
+  
+  describe('Initial properties', () => {
+    test('should exist', () => {
+      expect(timer).toBeDefined();
+    });
+
+    test('should have a worktime length == 25:00', () => {
+      expect(timer.worktimeLength).toBe(1500);
+    });
+
+    test('should have a breaktime length == 5:00', () => {
+      expect(timer.breaktimeLength).toBe(300);
+    });
+
+    test('should have a mode of work', () => {
+      expect(timer.mode).toBe(modes.WORK);
+    });
+
+    test('should have a time remaining == worktime', () => {
+      expect(timer.timeRemaining).toBe(timer.worktimeLength);
+    });
+  });
+
+  describe('Methods and operations', () => {
+    test('should change the worktime', () => {
+      timer.worktimeLength = 500;
+
+      expect(timer.worktimeLength).toBe(500);
+    });
+
+    test('should change the breaktime', () => {
+      timer.breaktimeLength = 500;
+
+      expect(timer.breaktimeLength).toBe(500);
+    });
+
+    describe('reset', () => {
+      test('should reset the time', () => {
+        timer.timeRemaining = 3;
+        timer.reset();
+  
+        expect(timer.timeRemaining).toBe(timer.worktimeLength);
+      });
+      
+      test('should reset the mode', () => {
+        timer.mode = modes.BREAK;
+        timer.reset();
+
+        expect(timer.mode).toBe(modes.WORK);
+      });
+    });
+
+    describe('start', () => {
+      test('should have the right time after one second', () => {
+        timer = new Timer();
+        const timeBefore = timer.timeRemaining;
+        timer.start();
+        jest.advanceTimersByTime(1000);
+
+        expect(timer.timeRemaining).toBe(timeBefore - 1);
+      });
+
+      test('should have the right time after a minute', () => {
+        timer = new Timer();
+        const timeBefore = timer.timeRemaining;
+        timer.start();
+        jest.advanceTimersByTime(60000);
+
+        expect(timer.timeRemaining).toBe(timeBefore - 60)
+      });
+
+      test('should have the right time after 10 minutes', () => {
+        timer = new Timer();
+        const timeBefore = timer.timeRemaining;
+        timer.start();
+        jest.advanceTimersByTime(600000);
+
+        expect(timer.timeRemaining).toBe(timeBefore - 600)
+      });
+
+      test('should raise the timeUpdated event each tick', () => {
+        timer = new Timer();
+        const f = jest.fn();
+        timer.timeUpdated = f;
+        timer.start();
+        jest.advanceTimersByTime(1000);
+
+        expect(f).toHaveBeenCalled();
+      });
+
+      test('should NOT go past 0 time remaining', () => {
+        timer = new Timer();
+        timer.start();
+        jest.advanceTimersByTime(1501000);
+
+        expect(timer.timeRemaining).toBe(0);
+      });
+
+      test('should only start once', () => {
+        timer = new Timer();
+        timer.start();
+        timer.start();
+        jest.advanceTimersByTime(1000);
+
+        expect(timer.timeRemaining).toBe(1499);
+      });
+    });
+
+    describe('switchMode', () => {
+      test('should switch from work to break', () => {
+        timer.reset();
+        timer.switchMode();
+
+        expect(timer.mode).toBe(modes.BREAK);
+      });
+
+      test('should switch from break to work', () => {
+        timer.reset();
+        timer.mode = modes.BREAK;
+        timer.switchMode();
+
+        expect(timer.mode).toBe(modes.WORK);
+      });
+    });
+  });
 });
